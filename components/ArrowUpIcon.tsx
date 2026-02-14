488ab7776047ec74ef42d97c3aa9d13ac40be801
Roqeeb <shafiriyuroqeeb@gmail.com>
2026-02-14 02:46:51 +0100

    added designs for buttons and icons and further designs

--- components/ArrowUpIcon.tsx
+++ components/ArrowUpIcon.tsx
@@ -2,7 +2,7 @@ import { ArrowUp } from "lucide-react";
 
 export default function ArrowUpIcon() {
   return (
-    <div className="bg-gray-900 sticky bottom-0 right-0 p-3 inline-block">
+    <div className="bg-gray-900 sticky m-1 bottom-1 right-1 p-4 inline-flex">
       <ArrowUp className="w-6 h-6 text-white" />
     </div>
   );
